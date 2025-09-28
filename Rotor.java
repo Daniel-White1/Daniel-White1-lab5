fb30693b8e90d5dd7e214a455712e4a9c9f0e613
Daniel White <daniel.white1@gwmail.gwu.edu>
2025-09-28 19:43:41 -0400

    added the roation script to rotate

--- Rotor.java
+++ Rotor.java
@@ -13,7 +13,16 @@ public class Rotor {
     }
     
     public boolean rotate(){
+        //create a temprorary array to store the rotated string
+        char[] temp = new char[27];
+        //rotor char
+        char[] rotor_char = this.rotorValues.toCharArray();
         if (this.rotorValues.charAt(0) == this.startChar) {
+            for(int i = 0; i < 26; ++i){
+                temp[i+1] = rotor_char[i];
+            }
+            temp[0] = temp[26];
+            this.rotorValues = temp.toString();
             return true;
         }
         return false;
