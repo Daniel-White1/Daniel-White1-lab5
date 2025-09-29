c4624aa8f18d531f0c5be3b932daf847417190d6
Daniel White <daniel.white1@gwmail.gwu.edu>
2025-09-29 00:43:13 -0400

    made enigma more clear

--- Enigma.java
+++ Enigma.java
@@ -21,23 +21,22 @@ public class Enigma{
 
     public String decrypt(String message){        
         //so this is just encyption but backwards pretty much
-        String result = new String();
+        String result = "";
         for(int i = 0; i < message.length(); i++){
             //gets the character we are encypting
-            char e_char = message.charAt(i);
+            char encryptedChar = message.charAt(i);
             
             //next we have to go though each rotor but backwards
             //ie: find character on outer -> find its index
             //find the character in the middle ring in the same index
             //then find the same character from the middle ring in the outer ring
-            //finally grab that outer index location and find the character that is in the same index in the inner ring
-            int index = rotors[2].indexOf(e_char);
-            e_char = rotors[1].charAt(index);
-            index = rotors[1].indexOf(e_char);
-            e_char = rotors[2].charAt(index);
-
+            //finally grab that outer index location and find the character that is in the same index in the inner layer
+            int index1 = rotors[2].indexOf(encryptedChar);
+            char middleChar = rotors[1].charAt(index1);
+            int index2 = rotors[2].indexOf(middleChar);
+            char innerChar = rotors[0].charAt(index2);
             //next add the new char to result
-            result = result + e_char;
+            result = result + innerChar;
 
             //then rotate the rotors
             rotate();
@@ -50,23 +49,23 @@ public class Enigma{
 
     //im going to work here first because in my mind its easier to write encryption first
     public String encrypt(String message){
-        String result = new String();
+        String result = "";
         for(int i = 0; i < message.length(); i++){
             //gets the character we are encypting
-            char e_char = message.charAt(i);
+            char startChar = message.charAt(i);
             
             //next we have to go though each rotor.
             //ie: find character on inner -> find its index
             //find the character in the outer ring in the same index
             //then find the same character from the outer ring in the middle ring
             //finally grab that middle index location and find the character that is in the same index in the outer ring
-            int index = rotors[0].indexOf(e_char);
-            e_char = rotors[2].charAt(index);
-            index = rotors[1].indexOf(e_char);
-            e_char = rotors[2].charAt(index);
+            int index1 = rotors[0].indexOf(startChar);
+            char outerChar = rotors[2].charAt(index1);
+            int index2 = rotors[1].indexOf(outerChar);
+            char encryptedChar = rotors[2].charAt(index2);
 
             //next add the new char to result
-            result = result + e_char;
+            result = result + encryptedChar;
 
             //then rotate the rotors
             rotate();
