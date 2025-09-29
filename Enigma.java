5a36e45e6e6d030d3f30984053ad78aa955f731b
Daniel White <daniel.white1@gwmail.gwu.edu>
2025-09-28 23:50:35 -0400

    made the decryption method

--- Enigma.java
+++ Enigma.java
@@ -20,7 +20,31 @@ public class Enigma{
 
 
     public String decrypt(String message){        
-        //TODO
+        //so this is just encyption but backwards pretty much
+        String result = new String();
+        for(int i = 0; i < message.length(); i++){
+            //gets the character we are encypting
+            char e_char = message.charAt(i);
+            
+            //next we have to go though each rotor but backwards
+            //ie: find character on outer -> find its index
+            //find the character in the middle ring in the same index
+            //then find the same character from the middle ring in the outer ring
+            //finally grab that outer index location and find the character that is in the same index in the inner ring
+            int index = rotors[2].indexOf(e_char);
+            e_char = rotors[1].charAt(index);
+            index = rotors[1].indexOf(e_char);
+            e_char = rotors[2].charAt(index);
+
+            //next add the new char to result
+            result = result + e_char;
+
+            //then rotate the rotors
+            rotate();
+        }
+        //finally return the resulting string
+        return result;
+        
     }
 
 
@@ -39,7 +63,7 @@ public class Enigma{
             int index = rotors[0].indexOf(e_char);
             e_char = rotors[2].charAt(index);
             index = rotors[1].indexOf(e_char);
-            e_char = rotors[2].charAt(e_char);
+            e_char = rotors[2].charAt(index);
 
             //next add the new char to result
             result = result + e_char;
